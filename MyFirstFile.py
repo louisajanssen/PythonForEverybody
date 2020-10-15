bf2d62b36527bf4a6563cdef7bbe0ee24cd53fea
louisajanssen <67242249+louisajanssen@users.noreply.github.com>
2020-10-15 11:16:43 -0700

    score program

--- MyFirstFile.py
+++ MyFirstFile.py
@@ -18,3 +18,21 @@ print('Blastoff!')
 
 #== equal to -> = assignment
 #!= not equal
+
+# program to prompt for a score between 0.0 and 1.0. Print error if score out of range.
+score = input("Enter Score: ")
+s =  float(score)
+x = 'Error'
+if s >= 0.9:
+	x = 'A'
+elif s >=0.8:
+	x='B'
+elif s >=0.7:
+	x='C'
+elif s >= 0.6:
+	x='D'
+elif s < .6:
+	x ='F'
+else:
+	x ="Out of Range"
+print (x)
