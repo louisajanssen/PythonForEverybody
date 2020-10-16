129872d5da408ea3e922e127b9c8f29aa27876e6
louisajanssen <67242249+louisajanssen@users.noreply.github.com>
2020-10-16 16:36:56 -0700

    problem 4.6

--- MyFirstFile.py
+++ MyFirstFile.py
@@ -66,3 +66,18 @@ def greet():
 
 print(greet()), "Glenn")
 print(greet()), "Sally")
+
+# Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Pay should be the normal rate for hours up to 40 and time-and-a-half for the hourly rate for all hours worked above 40 hours.
+def computepay(h,r):
+    if (h < 40):
+    	pay = h*r
+    elif h > 40 :
+    	pay = 40 * r + (h-40) * r * 1.5
+    return pay
+
+hrs = raw_input("Enter Hours:")
+h = float(hrs)
+rate = raw_input("Enter rate:")
+r = float(rate)
+p = computepay(h,r)
+print("Pay",p)
